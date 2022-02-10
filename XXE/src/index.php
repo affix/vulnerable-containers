efc03437ca4e5788ff47a7ba9d78c3b05c8bef08
Keiran Smith <contact@keiran.scot>
2022-02-10 23:30:12 +0000

    XXE Tutorial

---
+++ XXE/src/index.php
@@ -0,0 +1,10 @@
+<?php
+libxml_disable_entity_loader (false);
+$xmlfile = file_get_contents('php://input');
+$dom = new DOMDocument();
+$dom->loadXML($xmlfile, LIBXML_NOENT | LIBXML_DTDLOAD);
+$info = simplexml_import_dom($dom);
+$name = $info->name;
+$email = $info->email;
+
+echo "Sorry, $email is already registered!";
